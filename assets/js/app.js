dce45b02da04dbf4cc723121f86c2d038fc77a6d
Antoine <antoine@aymeric-dev.local>
2020-06-05 10:37:50 +0200

    fix css

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
+import "../css/tailwind.min.css"
 import "../css/app.css"
 
 // webpack automatically bundles all modules in your
